4c0568aea34031d547854bf615c937c062e35154
MrSuhar <56634152+MrSuhar@users.noreply.github.com>
2021-02-07 18:45:31 +0100

    Fixed racket physics, added pause

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ glob_height=(monitor_size.current_h-70)
 
 #creating game window
 screen=pygame.display.set_mode((glob_width,glob_height))
-pygame.display.set_caption("PONGER v 0.01")
+pygame.display.set_caption("PONGER v 0.02")
 
 #loading background image,scaling to monitor
 bckgrnd=pygame.image.load("TEXTURES/space.jpg")
@@ -39,6 +39,9 @@ if random.randint(0,3)==1:
 #point counters
 myfont=pygame.font.SysFont('Arial',30)
 
+#game pause boolean
+paused = False
+
 #MAIN LOOP
 run=True
 while run:
@@ -51,6 +54,9 @@ while run:
 			#escape turns game off
 			if event.key==pygame.K_ESCAPE:
 				run=False
+			#p pauses game	
+			if event.key==pygame.K_p:
+				paused = not paused
 
 		#player movement
 		pressed_keys=pygame.key.get_pressed()
@@ -67,47 +73,50 @@ while run:
 				P2_y+=10
 				if P2_y>((5/6)*glob_height):P2_y=((5/6)*glob_height)
 
-	#Ball movement
-	if Ball_y<Ball_radius:Ball_up=False
-	if Ball_y>(glob_height-Ball_radius):Ball_up=True
 
-	if Ball_up: Ball_y-=Ball_speed
-	else :Ball_y+=Ball_speed
+	#pausing game
+	if not paused:
+		#Ball movement
+		if Ball_y<Ball_radius:Ball_up=False
+		if Ball_y>(glob_height-Ball_radius):Ball_up=True
 
-	#hitting left racket
-	if Ball_x<Ball_radius:
-		if (Ball_y+Ball_radius)>=P1_y:
-			if Ball_y<=(P1_y+Ball_radius+glob_height/6):
-				Ball_right=True
-			else: 
-				Ball_x=glob_width/2		
+		if Ball_up: Ball_y-=Ball_speed
+		else :Ball_y+=Ball_speed
+
+		#hitting left racket
+		if Ball_x<Ball_radius:
+			if (Ball_y+Ball_radius)>=P1_y:
+				if Ball_y<=(P1_y+Ball_radius+glob_height/6):
+					Ball_right=True
+			else:
+				Ball_x=glob_width/2
 				P2_points+=1
 
-	#hitting right racket	
-	if Ball_x>=(glob_width-Ball_radius):
-		if (Ball_y+Ball_radius)>=P2_y:
-			if Ball_y<=(P2_y+Ball_radius+glob_height/6):
-				Ball_right=False
-			else: 
+		#hitting right racket
+		if Ball_x>=(glob_width-Ball_radius):
+			if (Ball_y+Ball_radius)>=P2_y:
+				if Ball_y<=(P2_y+Ball_radius+glob_height/6):
+					Ball_right=False
+			else:
 				Ball_x=glob_width/2
-				P1_points+=1		
+				P1_points+=1
 
-	if Ball_right: Ball_x+=Ball_speed
-	else :Ball_x-=Ball_speed								
-	
-	#background graphics
-	screen.blit(bckgrnd,(0,0))
-	#players point counters
-	counter_P1=myfont.render(str(P1_points),False,(255,255,255))
-	counter_P2=myfont.render(str(P2_points),False,(255,255,255))
-	screen.blit(counter_P1,(glob_width/4,0))
-	screen.blit(counter_P2,((3/4)*glob_width,0))
+		if Ball_right: Ball_x+=Ball_speed
+		else :Ball_x-=Ball_speed
 
-	#drawing rackets
-	pygame.draw.rect(screen,(255,255,255),(0,P1_y,glob_width/100,glob_height/6))
-	pygame.draw.rect(screen,(255,255,255),((99/100)*glob_width,P2_y,glob_width/100,glob_height/6))
-	pygame.draw.circle(screen,(255,255,255),(Ball_x,Ball_y),Ball_radius*2,0)
-	pygame.display.update()
+		#background graphics
+		screen.blit(bckgrnd,(0,0))
+		#players point counters
+		counter_P1=myfont.render(str(P1_points),False,(255,255,255))
+		counter_P2=myfont.render(str(P2_points),False,(255,255,255))
+		screen.blit(counter_P1,(glob_width/4,0))
+		screen.blit(counter_P2,((3/4)*glob_width,0))
+
+		#drawing rackets
+		pygame.draw.rect(screen,(255,255,255),(0,P1_y,glob_width/100,glob_height/6))
+		pygame.draw.rect(screen,(255,255,255),((99/100)*glob_width,P2_y,glob_width/100,glob_height/6))
+		pygame.draw.circle(screen,(255,255,255),(Ball_x,Ball_y),Ball_radius*2,0)
+		pygame.display.update()
 
 
 #closing screen
